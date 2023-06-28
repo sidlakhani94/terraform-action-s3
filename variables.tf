ceaa427595de1af2523e73995f7de10e90adc71f
sidlakhani94 <lakhanisiddharth_007@yahoo.co.in>
2023-06-28 16:04:37 +0530

    edited variables.tf and run fmt

--- variables.tf
+++ variables.tf
@@ -1,9 +1,12 @@
 variable "aws_region" {
   description = "AWS Region"
   type        = string
+  default     = "us-east-1"
 }
 
 variable "s3_bucket" {
   description = "Name of the S3 bucket"
   type        = string
+  default     = "s3-by-github-action"
+
 }
